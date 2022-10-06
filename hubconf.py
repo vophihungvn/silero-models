d3ee24c498fe540f1e62901438543f20a3cab371
Hung Vo <vphungkhtn@gmail.com>
2022-10-06 15:41:30 +0700

    fix: curdir

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,6 @@
-import sys
-sys.path.append("../src")
-sys.path.append("src")
-sys.path.append("src/silero")
+import sys, os
+sys.path.append(os.path.dirname(__file__))
+
 from src.silero import (
     silero_stt,
     silero_tts,
